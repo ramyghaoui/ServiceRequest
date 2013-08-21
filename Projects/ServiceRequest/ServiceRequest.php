613bd8edba56d34fcf3cc63919d0b6caed7da552
Ramy El-Ghaoui <Ramy@DELL>
2013-08-21 22:55:07 +0300

    hotfixes in ServiceRequest (!loggedin)

--- Projects/ServiceRequest/ServiceRequest.php
+++ Projects/ServiceRequest/ServiceRequest.php
@@ -2,51 +2,60 @@
 
 	require 'Core.php';
 	require 'Connect.php';
-
-	if(isset($_POST['ID'])&&isset($_POST['Requested_by'])&&isset($_POST['Date'])&&isset($_POST['Urgency'])&&isset($_POST['ServiceType'])&&isset($_POST['Problem'])){
-		$ID = $_POST['ID'];
-		$Requested_by = $_POST['Requested_by'];
-		$Date = $_POST['Date'];
-		$Urgency = $_POST['Urgency'];
-		$ServiceType = $_POST['ServiceType'];
-		$Problem = $_POST['Problem'];
-		if(!empty($ID)&&!empty($Requested_by)&&!empty($Date)&&!empty($Urgency)&&!empty($Problem)){
-			$query = "INSERT INTO `servicerequests`(`Requested_by`, `Urgency`, `ServiceType`, `Problem`, `Status`,`ReceivedDate`) VALUES ('".$Requested_by."','".$Urgency."','".$ServiceType."','".mysql_real_escape_string($Problem)."','Awaiting','".$Date."')";
-			if($query_run = mysql_query($query)){
-				echo "<script>alert('Form submitted');</script>";
+	
+	if(loggedin()){
+		if(isset($_POST['ID'])&&isset($_POST['Requested_by'])&&isset($_POST['Date'])&&isset($_POST['Urgency'])&&isset($_POST['ServiceType'])&&isset($_POST['Problem'])){
+			$ID = $_POST['ID'];
+			$Requested_by = $_POST['Requested_by'];
+			$Date = $_POST['Date'];
+			$Urgency = $_POST['Urgency'];
+			$ServiceType = $_POST['ServiceType'];
+			$Problem = $_POST['Problem'];
+			if(!empty($ID)&&!empty($Requested_by)&&!empty($Date)&&!empty($Urgency)&&!empty($Problem)){
+				$query = "INSERT INTO `servicerequests`(`Requested_by`, `Urgency`, `ServiceType`, `Problem`, `Status`,`ReceivedDate`) VALUES ('".$Requested_by."','".$Urgency."','".$ServiceType."','".mysql_real_escape_string($Problem)."','Awaiting','".$Date."')";
+				if($query_run = mysql_query($query)){
+					echo "<script>alert('Form submitted');</script>";
+				}
+				else{
+					echo "<script>alert('Could not submit request');</script>";
+				}
 			}
 			else{
-				echo "<script>alert('Could not submit request');</script>";
+				echo "<script>alert('Please fill in required fields');</script>";
 			}
 		}
-		else{
-			echo "<script>alert('Please fill in required fields');</script>";
-		}
-	}
 
 ?>
 
-	<form action="ServiceRequest.php" method="POST">
-		<input type="hidden" name="ID" value="<?php echo $_SESSION['user_id']; ?>" readonly>
-		Requested_by:<br><input type="text" name="Requested_by" value="<?php echo getuserfield('Username'); ?>" readonly><br><br>
-		Date:<br><input type="text" name="Date" value="<?php echo date('d/m/Y g:i A', time()+10800); ?>" readonly><br><br>
-		Urgency:<br><input type="radio" name="Urgency" value="Low">Low</input>
-					<input type="radio" name="Urgency" value="Medium" checked=true>Medium</input>
-					<input type="radio" name="Urgency" value="High" >High</input><br><br>
+		<form action="ServiceRequest.php" method="POST">
+			<input type="hidden" name="ID" value="<?php echo $_SESSION['user_id']; ?>" readonly>
+			Requested_by:<br><input type="text" name="Requested_by" value="<?php echo getuserfield('Username'); ?>" readonly><br><br>
+			Date:<br><input type="text" name="Date" value="<?php echo date('d/m/Y g:i A', time()+10800); ?>" readonly><br><br>
+			Urgency:<br><input type="radio" name="Urgency" value="Low">Low</input>
+						<input type="radio" name="Urgency" value="Medium" checked=true>Medium</input>
+						<input type="radio" name="Urgency" value="High" >High</input><br><br>
 
 <?php
 
-		$query = "SELECT `ServiceType` FROM `servicetypes` ORDER BY `ServiceType`";
-		$query_run = mysql_query($query);
-		echo '<select name="ServiceType">';
-		while ($query_num_row = mysql_fetch_array($query_run)) {
-		   echo '<option value="'.$query_num_row['ID'].'">'.$query_num_row['ServiceType'].'</option>';
-		}
-		echo '</select><br><br>';
+			$query = "SELECT `ServiceType` FROM `servicetypes` ORDER BY `ServiceType`";
+			$query_run = mysql_query($query);
+			echo '<select name="ServiceType">';
+			while ($query_num_row = mysql_fetch_array($query_run)) {
+			   echo '<option value="'.$query_num_row['ID'].'">'.$query_num_row['ServiceType'].'</option>';
+			}
+			echo '</select><br><br>';
 
 ?>
 
-		Problem:<br><textarea name="Problem" rows="5" cols="50" maxlength="255"/></textarea><br><br>
-		<input type="submit" name="Send" value="Send">	
-	</form>
-	<a href="Index.php">Home</a><br>
+			Problem:<br><textarea name="Problem" rows="5" cols="50" maxlength="255"/></textarea><br><br>
+			<input type="submit" name="Send" value="Send">	
+		</form>
+		<a href="Index.php">Home</a><br>
+<?php
+
+	}
+	else{
+		echo 'Please <a href="Index.php">log in!</a>';
+	}
+
+?>
