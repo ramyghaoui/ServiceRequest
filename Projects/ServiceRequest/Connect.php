14a4b6f961647ce88cb2f029d480ce7458c6ed94
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-08-30 03:59:28 -0700

    Update Connect.php

--- Projects/ServiceRequest/Connect.php
+++ Projects/ServiceRequest/Connect.php
@@ -5,6 +5,7 @@
 	$mysql_pass = '';
 	$mysql_db = 'servicerequests';
 	$error_msg = 'Could not connect to Server and/or DataBase';
+	
 	if (!@mysql_connect($mysql_host, $mysql_user, $mysql_pass)||(!mysql_select_db($mysql_db))){
 		die($error_msg);
 	}
