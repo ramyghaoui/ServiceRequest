8892bf0c5fa0c4a540b917671bc191cfbcbab091
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-08-30 03:58:37 -0700

    Update Core.php

--- Projects/ServiceRequest/Core.php
+++ Projects/ServiceRequest/Core.php
@@ -1,6 +1,6 @@
 <?php
 
-	ob_start();
+	ob_start(); //buffering delay for SQL Connect before query so it will not return error
 	session_start();
 	$current_file = $_SERVER['SCRIPT_NAME'];
 
@@ -42,21 +42,39 @@
 			echo '<th>'.mysql_field_name($query_run, $i).'</th>';
 		}
 		echo '</tr>';
-		while ($row = mysql_fetch_assoc($query_run, MYSQL_NUM)) {
+		while ($row = mysql_fetch_assoc($query_run, MYSQL_NUM)){
 			echo '<tr>';
 			FOR ($i=0; $i<mysql_num_fields($query_run); $i++){
-				echo '<td>'.$row[$i].'</td>'; 
+				echo '<td>'.$row[$i].'</td>';
 			}
 			echo '</tr>';
 		}
 		echo '</table>';
 	}
-	
+
 	function select($query, $value){
 		$query_run = mysql_query($query);
+		echo '<select name="'.$value.'"><option value="--Select Option--">--Select Option--</option>';
 		while ($query_num_row = mysql_fetch_array($query_run)){
 			echo '<option value="'.$query_num_row[$value].'">'.$query_num_row[$value].'</option>';
 		}
+		echo '</select><br><br>';
+	}
+
+	function timecost($Date){
+		$TimeDiff = time()+10800-strtotime($Date);
+		$TimeCost = number_format (($TimeDiff/3600), 2, ".", "");
+		return $TimeCost.' hour(s)';
+	}
+
+	function solution($field){
+		$query = "SELECT `ID`, `Requested_by`, `Urgency`, `Status`, `ServiceType`, `Problem`, `Solution`, `SubmittedDate` FROM `servicerequests` WHERE `Status`='Awaiting'";
+		$query_run = mysql_query($query);
+		$query_num_rows = mysql_num_rows($query_run);
+		if($query_num_rows>=1){
+			$field = mysql_result($query_run, 0, $field);
+		}
+		return $field;
 	}
 
 ?>
