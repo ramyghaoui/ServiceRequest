3eca2bad1a7e24db24dc7b1a8fd65ca21b048024
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-09-01 06:37:56 -0700

    adding CSS Styles

--- Projects/ServiceRequest/Main.php
+++ Projects/ServiceRequest/Main.php
@@ -3,9 +3,20 @@
 	
 	
 ?>
-	
-	<a href="Index.php">Home</a><br>
-	<a href="ServiceRequest.php">Service Request</a><br>
-	<a href="Solution.php">Solution</a><br>
-	<a href="Reports.php">Reports</a><br>
-	<a href="logout.php">Log out</a>
+
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Main Form</title>
+			<link rel="stylesheet" type="text/css" href="styling.css"/>
+		</head>
+			<body>
+				<h1>Welcome to Service Request Web Application</h1>
+				<h2>Please pick up a task!</h2>
+				<a href="Index.php">Home</a><br>
+				<a href="ServiceRequest.php">Service Request</a><br>
+				<a href="Solution.php">Solution</a><br>
+				<a href="Reports.php">Reports</a><br>
+				<a href="logout.php">Log out</a>
+			</body>
+	</html> 
