24661cfb4318573b08bc6d18f644e143d2d3af0d
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-09-01 06:38:55 -0700

    Adding CSS Styles

--- Projects/ServiceRequest/RegistrationSuccess.php
+++ Projects/ServiceRequest/RegistrationSuccess.php
@@ -1,5 +1,18 @@
+
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>LogIn Form</title>
+			<link rel="stylesheet" type="text/css" href="styling.css"/>
+		</head>
+			<body>
+				<h1>Welcome to Service Request Web Application</h1>
+				<h2>Registration Success!</h2>
+			</body>
+	</html>
+
 <?php
 
-	echo 'You have successfully registered! Back to <a href="Index.php">Home</a><br>';
+	echo 'You have successfully registered! Proceed to home page so you can <a href="Index.php">log in</a><br>';
 
 ?>
