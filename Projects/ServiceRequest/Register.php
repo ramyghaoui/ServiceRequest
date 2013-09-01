c6900ac4585841b0e637a610a0117348dd87bcb8
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-09-01 06:46:46 -0700

    Added CSS Styles

--- Projects/ServiceRequest/Register.php
+++ Projects/ServiceRequest/Register.php
@@ -46,34 +46,45 @@
 
 ?>
 
-		<form action="Register.php" method="POST">
-			Username:<br><input type="text" name="username" maxlength="20" value="<?php if(isset($username)){ echo $username; } ?>"><br><br>
-			Password:<br><input type="password" name="password"><br><br>
-			Re-enter password:<br><input type="password" name="re_password"><br><br>
-			First Name:<br><input type="text" name="firstname" maxlength="20" value="<?php if(isset($firstname)){ echo $firstname; } ?>"><br><br>
-			Last Name:<br><input type="text" name="lastname" maxlength="20" value="<?php if(isset($lastname)){ echo $lastname; } ?>"><br><br>
-			E-mail:<br><input type="text" name="email" maxlength="20" value="<?php if(isset($email)){ echo $email; } ?>"><br><br>
-			Department:<br>
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Registration Form</title>
+			<link rel="stylesheet" type="text/css" href="styling.css"/>
+		</head>
+			<body>
+				<h1>Welcome to Service Request Web Application</h1>
+				<h2>Please fill in all fields to register with a new account!</h2>
+				<form action="Register.php" method="POST">
+					Username:<br><input class="inputfield" type="text" name="username" maxlength="20" value="<?php if(isset($username)){ echo $username; } ?>"><br><br>
+					Password:<br><input class="inputfield" type="password" name="password"><br><br>
+					Re-enter password:<br><input class="inputfield" type="password" name="re_password"><br><br>
+					First Name:<br><input class="inputfield" type="text" name="firstname" maxlength="20" value="<?php if(isset($firstname)){ echo $firstname; } ?>"><br><br>
+					Last Name:<br><input class="inputfield" type="text" name="lastname" maxlength="20" value="<?php if(isset($lastname)){ echo $lastname; } ?>"><br><br>
+					E-mail:<br><input class="inputfield" type="text" name="email" maxlength="20" value="<?php if(isset($email)){ echo $email; } ?>"><br><br>
+					Department:<br>
 
 <?php
 
-			$query = "SELECT `Department` FROM `departments` ORDER BY `Department`";
-			select($query, 'Department');
+						$query = "SELECT `Department` FROM `departments` ORDER BY `Department`";
+						select($query, 'Department', 'inputfield');
 
 ?>
 
-			Title:<br>
+					Title:<br>
 
 <?php
 		
-			$query = "SELECT `Title` FROM `titles` ORDER BY `Title`";
-			select($query, 'Title');
+						$query = "SELECT `Title` FROM `titles` ORDER BY `Title`";
+						select($query, 'Title', 'inputfield');
 
 ?>
 
-			<input type="submit" name="Register" value="Register">
-		</form>
-		<a href="Index.php">Home</a><br>
+					<input type="submit" name="Register" value="Register">
+				</form>
+				<a href="Index.php">Home</a><br>
+			</body>
+	</html>
 
 <?php
 
