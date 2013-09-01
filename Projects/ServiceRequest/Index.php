8ed12788b222066bb67f45b838131cf09dfdd878
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-09-01 06:40:14 -0700

    Adding CSS Styles

--- Projects/ServiceRequest/Index.php
+++ Projects/ServiceRequest/Index.php
@@ -3,6 +3,22 @@
 	require 'Core.php';
 	require 'Connect.php';
 
+?>
+
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>LogIn Form</title>
+			<link rel="stylesheet" type="text/css" href="styling.css"/>
+		</head>
+			<body class="bg">
+				<h1>Welcome to Service Request Web Application</h1>
+				<h2>Service Request home.</h2>
+			</body>
+	</html>
+
+<?php
+
 	if (loggedin()){
 		$firstname = getuserfield('FirstName');
 		$lastname = getuserfield('LastName');
