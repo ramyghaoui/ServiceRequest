dbc5b94a79d4339cf6b2cc9b8a1c9bc9f2be3557
Ramy El-Ghaoui <ramyghaoui@hotmail.com>
2013-09-01 06:48:46 -0700

    Added CSS Styles

--- Projects/ServiceRequest/LogInForm.php
+++ Projects/ServiceRequest/LogInForm.php
@@ -1,12 +1,15 @@
 <?php
 
 	if (isset($_POST['username'])&&isset($_POST['password'])){
+	
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 		$password_hash = md5($password);
+		
 		if(!empty ($username)&&!empty($password)){
 
 			$query = "SELECT `ID` FROM `users` WHERE `Username`='".mysql_real_escape_string($username)."' AND `Password`='".mysql_real_escape_string($password_hash)."'";
+			
 			if($query_run = mysql_query($query)){
 				$query_num_rows = mysql_num_rows($query_run);
 				
@@ -26,9 +29,17 @@
 	}
 
 ?>
-
-	<form action="<?php echo $current_file; ?>" method="POST">
-		Username: <input type="text" name="username"> Password: <input type="password" name="password">
-		<input type="submit" name="Log in"><br>
-		New user? <a href="Register.php">Register now!</a>
-	</form>
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<title>LogIn Form</title>
+			<link rel="stylesheet" type="text/css" href="styling.css"/>
+		</head>
+			<body>
+				<form action="<?php echo $current_file; ?>" method="POST">
+					Username: <input class="inputfield" type="text" name="username"> Password: <input class="inputfield" type="password" name="password">
+					<input type="submit" name="Log in"><br>
+					New user? <a href="Register.php">Register now!</a>
+				</form>
+			</body>
+	</html> 
